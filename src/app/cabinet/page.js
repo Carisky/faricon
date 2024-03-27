90c812e7f447823ede66d67ee251cc94f867dd87
Yehor <egorkorsun7@gmail.com>
2024-03-27 18:59:59 +0100

    user cabinet page added

---
+++ src/app/cabinet/page.js
@@ -0,0 +1,12 @@
+import PageWrapper from "../UI/components/PageWrapper/PageWraper";
+
+
+export default function Cabinet() {
+  return (
+    <PageWrapper>
+      <div>
+        hello from /Cabinet
+      </div>
+    </PageWrapper>
+  );
+}
