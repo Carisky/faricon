e57134821fb776830f9494454455b0149afe7662
Yehor <egorkorsun7@gmail.com>
2024-03-27 18:59:47 +0100

    request page added

---
+++ src/app/request/page.js
@@ -0,0 +1,12 @@
+import PageWrapper from "../UI/components/PageWrapper/PageWraper";
+
+
+export default function Request() {
+  return (
+    <PageWrapper>
+      <div>
+        hello from /Request
+      </div>
+    </PageWrapper>
+  );
+}
