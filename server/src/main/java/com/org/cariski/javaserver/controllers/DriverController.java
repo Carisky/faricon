c163e468f92a8770b6720cf2a838f2d6c68612bb
Yehor <egorkorsun7@gmail.com>
2024-04-10 01:00:24 +0200

    changed curl

--- server/src/main/java/com/org/cariski/javaserver/controllers/DriverController.java
+++ server/src/main/java/com/org/cariski/javaserver/controllers/DriverController.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/Drivers")
-@CrossOrigin(origins = {"http://172.17.0.2:3000"})
+@CrossOrigin(origins = {"http://localhost:3000"})
 public class DriverController {
 
     private final DriverService driverService;
