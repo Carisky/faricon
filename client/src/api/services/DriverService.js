c09eae575119accc2f0f1d7ac4f6f199bb40d13f
Yehor <egorkorsun7@gmail.com>
2024-04-11 02:36:10 +0200

    refactor methods

--- client/src/api/services/DriverService.js
+++ client/src/api/services/DriverService.js
@@ -3,7 +3,7 @@ import ApiConfig from '../ApiConfig';
 
 
 class DriverService {
-    static async getDriverByID(Id) {
+    static async findById(Id) {
         try {
             const response = await axios.get(`${ApiConfig.apiDriversAdress}${Id}`);
             return response.data;
@@ -13,12 +13,12 @@ class DriverService {
         }
     }
 
-    static async getAllDrivers() {
+    static async findAll() {
         try {
             const response = await axios.get(ApiConfig.apiDriversAdress);
             return response.data;
         } catch (error) {
-            console.error('Error fetching driver:', error);
+            console.error('Error fetching drivers:', error);
             throw error;
         }
     }
