6c75800ed73572ffeb0b8fb0aebdf7d2cfb1c40a
Yehor <egorkorsun7@gmail.com>
2024-04-11 02:35:00 +0200

    config for Requests and Destinations added

--- client/src/api/ApiConfig.js
+++ client/src/api/ApiConfig.js
@@ -3,7 +3,9 @@ const BaseLink = "http://localhost:8080";
 const ApiConfig = {
     apiAdress: BaseLink,
     apiDriversAdress: BaseLink+"/Drivers/",
-    apiCargoTypesAdress: BaseLink+"/CargoTypes/"
+    apiCargoTypesAdress: BaseLink+"/CargoTypes/",
+    apiRequestsAdress: BaseLink+"/Requests/",
+    apiDestinationsAdress: BaseLink+"/Destinations/",
 };
 
 export default ApiConfig;
