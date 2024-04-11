50a8a6a210d3b3f428f7a245f57325584f47805a
Yehor <egorkorsun7@gmail.com>
2024-04-11 02:35:28 +0200

    CargoTypeService for find all CargoTypes added

--- client/src/api/services/CargoTypeService.js
+++ client/src/api/services/CargoTypeService.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import ApiConfig from '../ApiConfig';
 
 class CargoTypeService {
-    static async getAllCargoTypes() {
+    static async findAll() {
         try {
             const response = await axios.get(ApiConfig.apiCargoTypesAdress);
             return response.data;
