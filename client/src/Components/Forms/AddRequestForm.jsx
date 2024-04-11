5b3b82bc3d29e76a429ce3b372d5bb7227760cee
Yehor <egorkorsun7@gmail.com>
2024-04-11 02:36:57 +0200

    refactor and added logic for create new requests

--- client/src/Components/Forms/AddRequestForm.jsx
+++ client/src/Components/Forms/AddRequestForm.jsx
@@ -1,60 +1,92 @@
-import React from 'react';
-import { useForm } from 'react-hook-form';
-import { styled } from '@mui/material/styles';
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
-
-const CssTextField = styled(TextField)({
-    '& .MuiOutlinedInput-input': {
-        color: '#fff',
-      },
-      '& .MuiInputLabel-root': {
-        color: '#fff',
-      },
-    '& label.Mui-focused': {
-      color: '#3283a8',
-    },
-    '& .MuiInput-underline:after': {
-      borderBottomColor: '#B2BAC2',
-    },
-    '& .MuiOutlinedInput-root': {
-      '& fieldset': {
-        borderColor: '#E0E3E7',
-      },
-      '&:hover fieldset': {
-        borderColor: '#B2BAC2',
-      },
-      '&.Mui-focused fieldset': {
-        borderColor: '#3283a8',
-      },
-    },
-  });
+import { useState, useEffect } from "react";
+import { useForm } from "react-hook-form";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import MenuItem from "@mui/material/MenuItem";
+import CargoTypeService from "../../api/services/CargoTypeService";
+import DestinationService from "../../api/services/DestinationService";
+import RequestService from "../../api/services/RequestService";
 
 function AddRequestForm() {
+  const [cargoTypes, setCargoTypes] = useState([]);
+  const [destinations, setDestinations] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await CargoTypeService.findAll();
+        setCargoTypes(response.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await DestinationService.findAll();
+        setDestinations(response.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+    fetchData();
+  }, []);
+
+
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm();
 
-  const onSubmit = (data) => console.log(data);
+  const onSubmit = async (data) => {
+    try {
+      await RequestService.create(data);
+    } catch (error) {
+      console.error("Error creating request:", error);
+    }
+  };
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <CssTextField
-        label="Example"
-        {...register("example")}
+      <TextField
+        label="Cargo Quantity"
+        {...register("cargo_quantity")}
         fullWidth
         margin="normal"
+        error={!!errors.cargo_quantity}
+        type="number"
       />
+      <TextField
+        select
+        label="Cargo Type"
+        {...register("cargo_type")}
+        fullWidth
+        margin="normal"
+      >
+        {cargoTypes.map((cargoType) => (
+          <MenuItem key={cargoType.id} value={cargoType.id}>
+            {cargoType.name}
+          </MenuItem>
+        ))}
+      </TextField>
 
-      <CssTextField
-        label="Example Required"
-        {...register("exampleRequired")}
+      <TextField
+        select
+        label="Destination"
+        {...register("destination_id")}
         fullWidth
         margin="normal"
-        error={!!errors.exampleRequired}
-      />
+      >
+        {destinations.map((destination) => (
+          <MenuItem key={destination.id} value={destination.id}>
+            {destination.name}
+          </MenuItem>
+        ))}
+      </TextField>
 
       <Button type="submit" variant="contained" color="primary">
         Submit
