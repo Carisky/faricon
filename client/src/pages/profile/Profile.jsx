10a8a96862016391fabf5bfc5dfb5f48cf37d811
Yehor <egorkorsun7@gmail.com>
2024-04-15 21:59:55 +0200

    display user info

--- client/src/pages/profile/Profile.jsx
+++ client/src/pages/profile/Profile.jsx
@@ -1,21 +1,22 @@
 import styles from "../page.module.css";
 import Layout from "../layout/Layout";
-import { useAuth0 } from "@auth0/auth0-react";
 import Logout from "./Logout";
-
+import { useSelector } from "react-redux";
+import { Box } from "@mui/material";
 function Profile() {
-  const { user, isAuthenticated } = useAuth0();
-  console.log(user);
+
+  const userState = useSelector((state) => state.user);
+
   return (
     <Layout>
       <div className={styles.page}>
-        {isAuthenticated && (
-          <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
-            <div style={{ margin: "auto", width: "min-content" }}>{user.given_name}</div>
-            <img style={{ margin: "auto" }} src={user.picture} alt="User Profile" />
-            <Logout />
-          </div>
-        )}
+        <Box>
+          name : {userState.name}
+        </Box>
+        <Box>
+          role : {userState.role}
+        </Box>
+        <Logout/>
       </div>
     </Layout>
   );
