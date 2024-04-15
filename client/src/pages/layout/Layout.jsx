50cb4a20b858fdf96dd7c116049e0adfc8a7a89e
Yehor <egorkorsun7@gmail.com>
2024-04-15 22:01:49 +0200

    USER role for RBAC on frontend

--- client/src/pages/layout/Layout.jsx
+++ client/src/pages/layout/Layout.jsx
@@ -1,14 +1,21 @@
-import React from 'react'
-import styles from "./styles.module.css"
-import Navigation from "../../Components/Navigation/Navigation"
-
-export default function Layout({children}) {
+import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import styles from "./styles.module.css";
+import {useNavigate} from "react-router-dom"
+import Navigation from '../../Components/Navigation/Navigation';
+export default function Layout({ children }) {
+  const user = useSelector(state => state.user);
+  const navigate = useNavigate();
+  useEffect(() => {
+    console.log(user)
+    if (user.role !== "USER") {
+      navigate("/access-denied")
+    }
+  }, [user.role,navigate]);
   return (
-
     <div className={styles.layout}>
-      <Navigation/>
+      <Navigation />
       {children}
     </div>
-
-  )
+  );
 }
